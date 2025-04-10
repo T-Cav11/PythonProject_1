34f0c147eaac9bfb79223bb6385c5e97bba1faab
thomas <thomascavanagh98@gmail.com>
2025-04-10 12:09:41 +0100

    Adding functionality yo the "Add" button.

--- GUI.py
+++ GUI.py
@@ -2,11 +2,27 @@ import Functions
 import FreeSimpleGUI as sg
 
 label =sg.Text("Type in a task")
-input_box = sg.InputText(tooltip="Enter task: ")
+input_box = sg.InputText(tooltip="Enter task: ", key = "task")
 add_task_button = sg.Button("Add")
 
-window = sg.Window("Tasks to be completed", [[label], [input_box, add_task_button]])
-window.read()
+window = sg.Window("Tasks to be completed",
+                   [[label], [input_box, add_task_button]],
+                   font= ("Helvetica", 11))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            tasks = Functions.get_tasks()
+            new_task = values["task"] + "\n"
+            tasks.append(new_task)
+            Functions.write_tasks(tasks)
+        case sg.WIN_CLOSED:
+            break
+
+
 window.close()
 
 
